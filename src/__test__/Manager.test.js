87a5e9eea49bb0ab8fef10a9a388402a0b518ac3
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-07-06 13:39:38 -0700

    got my manager tests all green!

--- src/__test__/Manager.test.js
+++ src/__test__/Manager.test.js
@@ -1,10 +1,31 @@
 const Manager = require('../lib/Manager');
-const Manager = new Manager('Frank', '343', 'frankocon@gmail.com', '117');
+const manager = new Manager('Frank', '343', 'frankocon@gmail.com', '117');
 
-test('check if the constructor are displayed', () => {
-    expect(manager.name).toBE('Frank');
-    expect(manager.id).toBE('343');
-    expect(manager.email).toBE('frankocon@gmail.com');
-    expect(manager.officeNumber).toBE('117');
+test('check if info from the constructor are displayed', () => {
+    expect(manager.name).toBe('Frank');
+    expect(manager.id).toBe('343');
+    expect(manager.email).toBe('frankocon@gmail.com');
+    expect(manager.officeNumber).toBe('117');
 }); 
+
+test('to see if we can get name with getName()', () => {
+    expect(manager.getName()).toBe('Frank');
+});
+
+test('try to get manager id through the getId()', () => {
+    expect(manager.getId()).toBe('343');
+});
+
+test('try to get the email with getEmail()', () => {
+    expect(manager.getEmail()).toBe('frankocon@gmail.com');
+});
+
+test('try to get office number with getOfficeNumber()', () => {
+    expect(manager.getOfficeNumber()).toBe('117');
+})
+
+test('try to get role with getRole()', () => {
+    expect(manager.getRole()).toBe('Manager');
+})
+
 
--- src/lib/Manager.js
+++ src/lib/Manager.js
@@ -6,26 +6,18 @@ const inquirer = require('inquirer');
 
 //name id email getName() getId() getEmail() getRole() 
 class Manager extends Employee {
-    constructor(name, id, officeNumber, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+     
         this.officeNumber =officeNumber;
     }
-    
-getName() {
-    return this.name;
-}
-getId() {
-    return this.id;
-}
-getEmail() {
-    return this.email;
-}
-getRole() {
+
+        getRole() {
     return "Manager";
 }
-   
+getOfficeNumber() {
+    return this.officeNumber;
+}
      
 };
 
